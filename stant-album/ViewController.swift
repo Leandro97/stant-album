f618e8261d57a6f2bcae9ef7c2bb8f30cba05d25
Leandro97 <leandro97.martins@gmail.com>
2019-10-01 12:29:20 -0300

    Initial Commit

---
+++ stant-album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stant-album
+//
+//  Created by Stant Macmini n04 on 01/10/19.
+//  Copyright © 2019 Stant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
