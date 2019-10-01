507ddc327a2c2c9c301b680fa5102ba0771cf79f
Leandro97 <leandro97.martins@gmail.com>
2019-10-01 13:10:22 -0300

    Basic elements added

--- stant-album/ViewController.swift
+++ stant-album/ViewController.swift
@@ -2,19 +2,25 @@
 //  ViewController.swift
 //  stant-album
 //
-//  Created by Stant Macmini n04 on 01/10/19.
+//  Created by Leandro Martins on 01/10/19.
 //  Copyright © 2019 Stant. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var albumCoverImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var newAlbumButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getNewAlbumAction(_ sender: Any) {
+    }
+    
 }
 
