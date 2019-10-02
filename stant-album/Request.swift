d3e97bf943018354211be90e7308c742e90d090a
Leandro97 <leandro97.martins@gmail.com>
2019-10-02 18:37:43 -0300

    Request structure

---
+++ stant-album/Request.swift
@@ -0,0 +1,24 @@
+//
+//  Request.swift
+//  stant-album
+//
+//  Created by Leandro Martins on 02/10/19.
+//  Copyright © 2019 Stant. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+let session = URLSession(configuration: .default)
+
+//Error tratement needed
+func makeRequest(getFromURL: String, requestCompletionHandler: @escaping (Data?, Error?) -> Void){
+    if let url = URL(string: getFromURL) {
+        session.dataTask(with: url) { (data, response, error) in
+            if let responseData = data {
+                requestCompletionHandler(responseData, nil)
+            }
+            }.resume()
+    }
+}
+
