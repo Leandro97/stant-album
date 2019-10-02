fe7f98ba1cbd04237c8291deb338ee572932eb5c
Leandro97 <leandro97.martins@gmail.com>
2019-10-02 18:48:42 -0300

    New album structure

--- stant-album/AlbumGenerator.swift
+++ stant-album/AlbumGenerator.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 struct AlbumGenerator {
-    var album1 = Album(artistName: "artist 1", recordLabel: "record 1", albumName: "album 1", albumCover: "birds.jpg")
-    var album2 = Album(artistName: "artist 2", recordLabel: "record 2", albumName: "album 2", albumCover: "cat.jpg")
-    var album3 = Album(artistName: "artist 3", recordLabel: "record 3", albumName: "album 3", albumCover: "dog_img.jpg")
-    var album4 = Album(artistName: "artist 4", recordLabel: "record 4", albumName: "album 4", albumCover: "happy_lamb.jpg")
-    var album5 = Album(artistName: "artist 5", recordLabel: "record 5", albumName: "album 5", albumCover: "road_image.jpg")
+    var album1 = Album(id: "1", artistName: "artist 1", recordLabel: "record 1", albumName: "album 1", albumCover: "birds.jpg")
+    var album2 = Album(id: "2", artistName: "artist 2", recordLabel: "record 2", albumName: "album 2", albumCover: "cat.jpg")
+    var album3 = Album(id: "3", artistName: "artist 3", recordLabel: "record 3", albumName: "album 3", albumCover: "dog_img.jpg")
+    var album4 = Album(id: "4", artistName: "artist 4", recordLabel: "record 4", albumName: "album 4", albumCover: "happy_lamb.jpg")
+    var album5 = Album(id: "5", artistName: "artist 5", recordLabel: "record 5", albumName: "album 5", albumCover: "road_image.jpg")
     var albumList: [Album]
 
     init() {
